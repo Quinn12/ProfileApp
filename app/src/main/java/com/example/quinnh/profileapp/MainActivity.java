db5df78aeddf625a2cf04bec54bc94a7764ee901
Quinn Hastings <qhastings@students.mchenry.edu>
2018-11-13 01:49:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/quinnh/profileapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.quinnh.profileapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button_Main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Profilw.class));
+            }
+        });
+    }
+}
